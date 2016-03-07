86937d3039bc1cecb2d21b53cc77dd1522ab2476
jeran <jerantfox@gmail.com>
2016-03-07 00:23:54 -0800

    Add back functionality to navigation button in toolbar.

--- app/src/main/java/com/jeranfox/peach/SignInActivity.java
+++ app/src/main/java/com/jeranfox/peach/SignInActivity.java
@@ -2,24 +2,37 @@ package com.jeranfox.peach;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Toolbar;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class SignInActivity extends AppCompatActivity {
 
+    @Bind(R.id.sign_in_toolbar)
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
         ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
     }
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
         overridePendingTransition(R.animator.translate_in_from_left, R.animator.translate_out_to_right);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        if (menuItem.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(menuItem);
     }
 }
