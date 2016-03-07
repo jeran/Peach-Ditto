232f62e012526015f12019daeb0ccf66f9466cb6
jeran <jerantfox@gmail.com>
2016-03-06 17:48:49 -0800

    Add butterknife to project.

--- app/src/main/java/com/jeranfox/peach/LoginActivity.java
+++ app/src/main/java/com/jeranfox/peach/LoginActivity.java
@@ -4,13 +4,19 @@ import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class LoginActivity extends AppCompatActivity {
+
+    @Bind(R.id.view_pager)
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+        ButterKnife.bind(this);
         viewPager.setAdapter(new LoginPagerAdapter(this));
     }
 }
