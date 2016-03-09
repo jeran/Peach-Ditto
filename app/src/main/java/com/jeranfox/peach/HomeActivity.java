ba8017cc0d247426b6ab36836fa41f6d2ae60c95
jeran <jerantfox@gmail.com>
2016-03-08 19:30:07 -0800

    Add empty HomeActivity.

---
+++ app/src/main/java/com/jeranfox/peach/HomeActivity.java
@@ -0,0 +1,21 @@
+package com.jeranfox.peach;
+
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Bind(R.id.home_view_pager)
+    ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
+    }
+}
