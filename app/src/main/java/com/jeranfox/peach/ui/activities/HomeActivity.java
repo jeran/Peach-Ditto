0e71227d1f15b07f7e2d8ea32b1e0e0b79577fbc
jeran <jerantfox@gmail.com>
2016-03-12 17:06:37 -0800

    Add Fab button to home activity.

--- app/src/main/java/com/jeranfox/peach/ui/activities/HomeActivity.java
+++ app/src/main/java/com/jeranfox/peach/ui/activities/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.jeranfox.peach.ui.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +10,7 @@ import com.jeranfox.peach.R;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -21,5 +23,10 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         ButterKnife.bind(this);
         viewPager.setAdapter(new HomePagerAdapter(this));
+    }
+
+    @OnClick(R.id.home_fab)
+    void openPersonalFeed() {
+        startActivity(new Intent(this, PersonalFeedActivity.class));
     }
 }
---
+++ app/src/main/java/com/jeranfox/peach/ui/activities/PersonalFeedActivity.java
@@ -0,0 +1,15 @@
+package com.jeranfox.peach.ui.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.jeranfox.peach.R;
+
+public class PersonalFeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_feed);
+    }
+}
