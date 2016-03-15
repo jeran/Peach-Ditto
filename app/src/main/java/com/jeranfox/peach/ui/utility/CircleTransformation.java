98fbc3d30aca9e7d44ba9280c7e5ffdac0566f1c
jeran <jerantfox@gmail.com>
2016-03-15 10:11:55 -0700

    Add null check to bitmap config.
    I'm not sure what the cause is, but we had a crash with a null bitmap
    config. I've seen this a bit in the past and it seems a good work around
    is to simply substitute our own config as needed.

--- app/src/main/java/com/jeranfox/peach/ui/utility/CircleTransformation.java
+++ app/src/main/java/com/jeranfox/peach/ui/utility/CircleTransformation.java
@@ -23,7 +23,8 @@ public class CircleTransformation implements Transformation {
             source.recycle();
         }
 
-        Bitmap bitmap = Bitmap.createBitmap(size, size, source.getConfig());
+        Bitmap.Config config = source.getConfig() == null ? Bitmap.Config.ARGB_8888 : source.getConfig();
+        Bitmap bitmap = Bitmap.createBitmap(size, size, config);
 
         Canvas canvas = new Canvas(bitmap);
         Paint paint = new Paint();
